6d406133b33619a9fde30e4c7c6b60ee93d7160c
AmzGrainRain <amzgrainrain@outlook.com>
2023-06-15 23:13:49 +0800

    添加一些注释

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,24 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        // 实例化
         Server app = new Server(80);
+        // 设置静态目录
         app.staticDir("D:\\Repo\\Express-For-Java\\static");
 
+        // 设置一个 get 请求类型的路由
         app.get("/login", (req, res) -> {
+            // 设置响应头
             res.setHeader("Content-Type", "text/html; charset=utf-8");
+            // 发送文本消息
             res.send("<h1>您传入了" + req.params.get("asd") + "</h1>");
         });
 
+        // 设置一个 post 请求类型的路由
         app.post("/register", (req, res) -> {
+            // 设置响应头
             res.setHeader("Content-Type", "text/html; charset=utf-8");
+            // 返回数据
             if (req.body.size() != 0) {
                 StringBuilder sb = new StringBuilder();
                 for (Map.Entry<String, String> pair : req.body.entrySet()) {
